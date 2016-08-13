4cca7b358455dcb4f7b2950ad0291554798b5030
abouthiroppy <git@about-hiroppy.com>
2016-08-13 10:33:04 +0900

    Insert author and license to package.json which was generated
    Add rc.

--- bin/npm.js
+++ bin/npm.js
@@ -3,16 +3,30 @@
 const _        = require('lodash');
 const fs       = require('fs');
 const path     = require('path');
+const rc       = require('rc');
 const spawn    = require('cross-spawn');
 const env      = require('../data/env');
 const tasks    = require('../data/tasks');
 const packages = require('../data/packages');
 
 module.exports = (projectName, projectPath) => {
+  const npmrc  = rc('npm');
+  const author = (() => {
+    const str = [];
+
+    if (npmrc['init.author.name'])  str.push(npmrc['init.author.name']);
+    if (npmrc['init.author.email']) str.push(`<${npmrc['init.author.email']}>`);
+    if (npmrc['init.author.url'])   str.push(`(${npmrc['init.author.url']})`);
+
+    return str.join(' ');
+  })();
+
   const packageInfo = {
     name: projectName,
     main: 'src/index.js',
+    author: author === '' ? undefined : author,
     version: '0.0.1',
+    license: npmrc['init.license'] === undefined ? 'ISC' : npmrc['init.license'],
     description: '',
   };
 
