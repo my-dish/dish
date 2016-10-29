48c71e16209903050139cae153564e9345808533
abouthiroppy <git@about-hiroppy.com>
2016-10-29 09:36:19 +0900

    feat(npm): add npm settings for react-library

---
+++ npm/react-library/env.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  ava: {
+    files: [
+      'test/**/*.js'
+    ],
+    concurrency: 5,
+    failFast: true,
+    tap: true,
+    require: [
+      'ignore-styles',
+      'babel-register'
+    ],
+    babel: 'inherit'
+  }
+};
---
+++ npm/react-library/packages.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  dependencies: [
+    'react',
+    'lodash.assign'
+  ],
+  devDependencies: [
+    'ava',
+    'conventional-changelog-cli'
+  ]
+};
