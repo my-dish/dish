04ab604ad995ce8ed2d8b6a793cf59db390e67af
abouthiroppy <git@about-hiroppy.com>
2017-01-02 21:24:47 +0900

    refactor(react-template): modify src/index.js
    Make use of webpack2.

--- templates/react-application/src/index.js
+++ templates/react-application/src/index.js
@@ -5,22 +5,19 @@ import App from './components/App';
 
 const rootEl = document.getElementById('root');
 
-ReactDOM.render((
-  <AppContainer>
-    <App />
-  </AppContainer>),
+const render = () => {
+  ReactDOM.render((
+    <AppContainer>
+      <App />
+    </AppContainer>),
   rootEl
-);
+  );
+};
+
+render();
 
 if (module.hot) {
   module.hot.accept('./components/App', () => {
-    const NextApp = require('./components/App').default;
-
-    ReactDOM.render((
-      <AppContainer>
-        <NextApp />
-      </AppContainer>),
-      rootEl
-    );
+    render();
   });
 }
