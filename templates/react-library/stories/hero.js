f74e1ebd45aac4ed2438fc26af6e2155b7635b9b
Yuta Hiroto <git@about-hiroppy.com>
2017-05-08 00:23:00 +0900

    feat(templates): remove templates from this repo
    https://github.com/my-dish

--- templates/react-library/stories/hero.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@kadira/storybook';
-import Hero from '../src';
-
-storiesOf('Hero', module).add('list', () => (
-  <Hero />
-));
--- templates/react-redux-application/src/middlewares/index.js
+++
@@ -1,1 +0,0 @@
-export checkName from './checkName';
