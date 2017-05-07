a44da0f92d390f1761d63fdfebcc9919eef69a25
Yuta Hiroto <git@about-hiroppy.com>
2017-05-08 00:39:00 +0900

    feat: allow to get templates from npm

--- bin/npm.js
+++ bin/npm.js
@@ -1,62 +1,78 @@
 'use strict';
 
-const fs       = require('fs');
-const path     = require('path');
-const rc       = require('rc');
-const spawn    = require('cross-spawn');
-const router   = require('./router');
+const fs    = require('fs');
+const path  = require('path');
+const rc    = require('rc');
+const spawn = require('cross-spawn');
 
-module.exports = (projectName, projectPath, templateType) => {
-  const npm    = router(templateType).npm;
+module.exports = (projectName, projectPath, templateURL) => {
+  if (process.env.NODE_ENV !== 'test') {
+
+    const commonURL = '@my-dish/template-common';
+
+    // install template
+    install([
+      'install',
+      commonURL,
+      templateURL
+    ]);
+
+    const npm = require(`${process.cwd()}/node_modules/${templateURL}/npm`);
+
+    createPackageJSON(npm, projectName, projectPath);
+
+    // install dependencies
+    if (npm.packages.dependencies) {
+      install([
+        'install',
+        '-S',
+        ...npm.packages.dependencies
+      ]);
+    }
+
+    // install devDependencies
+    if (npm.packages.devDependencies) {
+      install([
+        'install',
+        '-D',
+        ...npm.packages.devDependencies
+      ]);
+    }
+  }
+};
+
+/**
+ * will delete
+ */
+function createPackageJSON(npm, projectName, projectPath) {
   const npmrc  = rc('npm');
 
   const author = (() => {
     const str = [];
 
-    if (npmrc['init.author.url']) str.push(`(${npmrc['init.author.url']})`);
     if (npmrc['init.author.name']) str.push(npmrc['init.author.name']);
     if (npmrc['init.author.email']) str.push(`<${npmrc['init.author.email']}>`);
+    if (npmrc['init.author.url']) str.push(`(${npmrc['init.author.url']})`);
 
     return str.join(' ');
   })();
 
   const packageInfo = {
     name       : projectName,
-    main       : path.join(npm.setting.main, 'index.js'),
+    main       : npm.settings.main ? path.join(npm.settings.main, 'index.js') : '',
     author     : author === '' ? undefined : author,
     version    : '0.0.1',
     license    : npmrc['init.license'] === undefined ? 'ISC' : npmrc['init.license'],
     description: ''
   };
 
-  npm.setting.key.forEach((e) => packageInfo[Object.keys(e)[0]] = e[Object.keys(e)[0]]);
+  npm.settings.key.forEach((e) => {
+    packageInfo[Object.keys(e)[0]] = e[Object.keys(e)[0]];
+  });
 
   fs.writeFileSync(path.join(projectPath, 'package.json'),
     JSON.stringify(Object.assign(packageInfo, npm.tasks, npm.env), null, 2));
-
-  if (process.env.NODE_ENV !== 'test') {
-
-    // install template
-    install([
-      'install',
-      'my-dish'
-    ]);
-
-    // install dependencies
-    install([
-      'install',
-      '-S',
-      ...npm.packages.dependencies
-    ]);
-
-    // install devDependencies
-    install([
-      'install',
-      '-D',
-      ...npm.packages.devDependencies
-    ]);
-  }
-};
+}
 
 /**
  * @description execute install
--- bin/router.js
+++ bin/router.js
@@ -1,66 +1,28 @@
 'use strict';
 
-module.exports = (templateType) => {
-  switch(templateType) {
-    case 0:
-    case 'Web Application with React':
-      return {
-        id : 0,
-        npm: {
-          env     : require('../npm/react-application/env'),
-          tasks   : require('../npm/react-application/tasks'),
-          setting : require('../npm/react-application/setting'),
-          packages: require('../npm/react-application/packages')
-        },
-        templatePath: 'react-application'
-      };
-    case 1:
-    case 'React Library':
-      return {
-        id : 1,
-        npm: {
-          env     : require('../npm/react-library/env'),
-          tasks   : require('../npm/react-library/tasks'),
-          setting : require('../npm/react-library/setting'),
-          packages: require('../npm/react-library/packages')
-        },
-        templatePath: 'react-library'
-      };
-    case 2:
-    case 'CLI':
-      return {
-        id : 2,
-        npm: {
-          env     : require('../npm/cli/env'),
-          tasks   : require('../npm/cli/tasks'),
-          setting : require('../npm/cli/setting'),
-          packages: require('../npm/cli/packages')
-        },
-        templatePath: 'cli'
-      };
-    case 4:
-    case 'Web Application with React and Redux':
-      return {
-        id : 4,
-        npm: {
-          env     : require('../npm/react-redux-application/env'),
-          tasks   : require('../npm/react-redux-application/tasks'),
-          setting : require('../npm/react-redux-application/setting'),
-          packages: require('../npm/react-redux-application/packages')
-        },
-        templatePath: 'react-redux-application'
-      };
-    case 5:
-    case 'Plain':
-      return {
-        id : 5,
-        npm: {
-          env     : require('../npm/plain/env'),
-          tasks   : require('../npm/plain/tasks'),
-          setting : require('../npm/plain/setting'),
-          packages: require('../npm/plain/packages')
-        },
-        templatePath: 'plain'
-      };
+module.exports = [
+  {
+    name: 'Web Application with React and Redux',
+    url : '@my-dish/template-react-redux-application'
+  },
+  {
+    name: 'Web Application with React',
+    url : '@my-dish/template-react-application'
+  },
+  {
+    name: 'React Library',
+    url : '@my-dish/template-react-library'
+  },
+  {
+    name: 'CLI',
+    url : '@my-dish/template-cli'
+  },
+  {
+    name: 'Plain',
+    url : '@my-dish/template-plain'
+  },
+  {
+    name: 'Dish',
+    url : '@my-dish/template-dish'
   }
-};
+];
