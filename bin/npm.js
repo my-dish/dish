385a4d27f837b6fc8a041716dbe51056db5a0832
Yuta Hiroto <git@about-hiroppy.com>
2017-05-08 08:42:47 +0900

    feat(npm): make use of packer

--- bin/npm.js
+++ bin/npm.js
@@ -1,82 +1,30 @@
 'use strict';
 
-const fs    = require('fs');
-const path  = require('path');
-const rc    = require('rc');
-const spawn = require('cross-spawn');
+const fs     = require('fs');
+const path   = require('path');
+const packer = require('@my-dish/packer');
 
 module.exports = (projectName, projectPath, templateURL) => {
-  if (process.env.NODE_ENV !== 'test') {
 
-    const commonURL = '@my-dish/template-common';
+  // install template
+  packer.installTemplates(templateURL);
 
-    // install template
-    install([
-      'install',
-      commonURL,
-      templateURL
-    ]);
+  const npm = require(`${process.cwd()}/node_modules/${templateURL}/npm`);
 
-    const npm = require(`${process.cwd()}/node_modules/${templateURL}/npm`);
+  const packageJSON = packer.createPackageJSON(npm, projectName);
 
-    createPackageJSON(npm, projectName, projectPath);
+  fs.writeFileSync(path.join(projectPath, 'package.json'), packageJSON);
 
-    // install dependencies
-    if (npm.packages.dependencies) {
-      install([
-        'install',
-        '-S',
-        ...npm.packages.dependencies
-      ]);
-    }
+  let command = 'yarn';
 
-    // install devDependencies
-    if (npm.packages.devDependencies) {
-      install([
-        'install',
-        '-D',
-        ...npm.packages.devDependencies
-      ]);
-    }
+  packer.installPackages(
+    command,
+    npm.packages.dependencies,
+    npm.packages.devDependencies
+  );
+
+  // yarn overwrites node_modules so dish reinstalls templates
+  if (command === 'yarn') {
+    packer.installTemplates(templateURL);
   }
 };
-
-/**
- * will delete
- */
-function createPackageJSON(npm, projectName, projectPath) {
-  const npmrc  = rc('npm');
-
-  const author = (() => {
-    const str = [];
-
-    if (npmrc['init.author.name']) str.push(npmrc['init.author.name']);
-    if (npmrc['init.author.email']) str.push(`<${npmrc['init.author.email']}>`);
-    if (npmrc['init.author.url']) str.push(`(${npmrc['init.author.url']})`);
-
-    return str.join(' ');
-  })();
-
-  const packageInfo = {
-    name       : projectName,
-    main       : npm.settings.main ? path.join(npm.settings.main, 'index.js') : '',
-    author     : author === '' ? undefined : author,
-    version    : '0.0.1',
-    license    : npmrc['init.license'] === undefined ? 'ISC' : npmrc['init.license'],
-    description: ''
-  };
-
-  npm.settings.key.forEach((e) => {
-    packageInfo[Object.keys(e)[0]] = e[Object.keys(e)[0]];
-  });
-
-  fs.writeFileSync(path.join(projectPath, 'package.json'),
-    JSON.stringify(Object.assign(packageInfo, npm.tasks, npm.env), null, 2));
-}
-
-/**
- * @description execute install
- */
-function install(args) {
-  spawn.sync('npm', args, { stdio: 'inherit' });
-}
