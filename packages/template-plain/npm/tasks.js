fa4360ae261e6fa5f3f8323d245fba8ade0ed5f4
Yuta Hiroto <git@about-hiroppy.com>
2017-07-23 20:34:07 +0900

    feat(plain): delete debug task

--- packages/template-plain/npm/tasks.js
+++ packages/template-plain/npm/tasks.js
@@ -4,7 +4,6 @@ module.exports = {
   scripts: {
     lint     : 'eslint .',
     test     : 'nyc ava',
-    debug    : 'packer --debug',
     changelog: 'conventional-changelog -p angular -i CHANGELOG.md -s -r 0',
     postcover: 'nyc npm test && nyc report --reporter=text-lcov > coverage.lcov && codecov'
   }
