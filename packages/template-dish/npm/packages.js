8311da05f0684fa8896c215d19476122bc5a06ea
Yuta Hiroto <git@about-hiroppy.com>
2017-08-22 20:03:52 +0900

    wip

--- packages/template-dish/npm/packages.js
+++ packages/template-dish/npm/packages.js
@@ -1,4 +1,17 @@
 'use strict';
 
 module.exports = {
+  devDependencies: [
+    'ava',
+    'babel-preset-flow',
+    'babel-preset-latest',
+    'babel-preset-stage-1',
+    'conventional-changelog-cli',
+    'eslint',
+    'eslint-config-sky',
+    'flow-bin',
+    'flow-typed',
+    'nyc'
+  ],
+  dependencies: []
 };
