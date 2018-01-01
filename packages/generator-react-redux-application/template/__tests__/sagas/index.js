3402a354cf78460cf301f0985ff6c378317981a2
Yuta Hiroto <git@about-hiroppy.com>
2018-01-01 17:22:50 +0900

    feat(generator-react-redux-application): delete generator-react-redux-application

--- packages/generator-react-redux-application/template/__tests__/sagas/index.js
+++
@@ -1,9 +0,0 @@
-import rootSaga from '../../src/sagas';
-
-describe('root saga', () => {
-  it('should register sagas', () => {
-    const tasks = 1;
-
-    expect(rootSaga()._invoke().value.length).toEqual(tasks);
-  });
-});
